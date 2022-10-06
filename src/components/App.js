341cd142e11d4363c9bb27d19685608eaee326ee
Hitesh Pagare <hitesh.pagare@uplers.in>
2022-10-06 18:23:26 +0530

    Component Folder Structure Added and Styles added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import { Actors, Movies, MovieInformation, Profiles, NavBar } from '.';
+import UseStyles from './styles';
 
-const App = () => {
-  (
-    <div>App</div>
+function App() {
+  const classes = UseStyles();
+
+  return (
+    <div className={classes.root}>
+      <NavBar />
+      <CssBaseline />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/" element="Home" />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movieinformation" element={<MovieInformation />} />
+          <Route path="/profiles" element={<Profiles />} />
+          <Route path="*" element="Not Found" />
+        </Routes>
+      </main>
+    </div>
   );
-};
+}
+
 export default App;
